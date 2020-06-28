04a32330a6d9f0dc34fb8de32c628a756ac9d723
Anton Veldanov <anton_veldanov@intuit.com>
2020-06-27 19:20:28 -0700

    iOS

---
+++ String/ToLowerCase.playground/Contents.swift
@@ -0,0 +1,25 @@
+/*
+ 
+ Input: "Hello"
+ Output: "hello"
+ Example 2:
+
+ Input: "here"
+ Output: "here"
+ Example 3:
+
+ Input: "LOVELY"
+ Output: "lovely"
+ Accepted
+ 
+ 
+ 
+ */
+
+
+
+class Solution {
+    func toLowerCase(_ str: String) -> String {
+        
+    }
+}
