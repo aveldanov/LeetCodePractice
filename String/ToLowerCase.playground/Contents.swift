a842535b6a6d1ec5419e485f4deb032ac751afcf
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-27 20:00:23 -0700

    iOS

--- String/ToLowerCase.playground/Contents.swift
+++ String/ToLowerCase.playground/Contents.swift
@@ -21,7 +21,34 @@
 class Solution {
     func toLowerCase(_ str: String) -> String {
         
+      let arr = Array(str)
+      
+      let upprange = 65...90
+      //65-90
+      //97-100
+      
+//      print(arr.map{upprange.contains(Int($0.unicodeScalars.first!.value)) ? helperToChar(Int($0.unicodeScalars.first!.value)+32) : helperToChar(Int($0.unicodeScalars.first!.value))
+//        }.joined(separator: ""))
+//
+
+      
+//      print("b".utf8CString.first!)
+//      print(arr.map{$0.lowercased()}.joined(separator: ""))
+      
+      return arr.map{upprange.contains(Int($0.unicodeScalars.first!.value)) ? helperToChar(Int($0.unicodeScalars.first!.value)+32) : helperToChar(Int($0.unicodeScalars.first!.value))
+      }.joined(separator: "")
     }
+  func helperToChar(_ charCode:Int)->String{
+    
+    
+    return String(UnicodeScalar(UInt8(charCode)))
+  }
+  
+
 }
 
+
 let solution = Solution()
+
+
+print(solution.toLowerCase("here"))
