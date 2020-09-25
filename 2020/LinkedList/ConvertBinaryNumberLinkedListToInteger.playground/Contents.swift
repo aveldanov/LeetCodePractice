214c5fc998fa9b574c076fdbb154dd84bdd26ac1
Anton Veldanov <anton.veldanov@gmail.com>
2020-09-24 20:48:23 -0700

    iOS

---
+++ 2020/LinkedList/ConvertBinaryNumberLinkedListToInteger.playground/Contents.swift
@@ -0,0 +1,132 @@
+/*
+ Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+ Return the decimal value of the number in the linked list.
+
+  
+
+ Example 1:
+
+
+ Input: head = [1,0,1]
+ Output: 5
+ Explanation: (101) in base 2 = (5) in base 10
+ Example 2:
+
+ Input: head = [0]
+ Output: 0
+ Example 3:
+
+ Input: head = [1]
+ Output: 1
+ Example 4:
+
+ Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+ Output: 18880
+ Example 5:
+
+ Input: head = [0,0]
+ Output: 0
+ 
+ 
+ 
+ */
+
+
+
+//  Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+//    public init() { self.val = 0; self.next = nil; }
+//    public init(_ val: Int) {
+//        self.val = val;
+//        self.next = nil;
+//
+//    }
+    public init(_ val: Int, _ next: ListNode?) {
+        self.val = val;
+        self.next = next;
+        
+    }
+}
+
+
+class LinkedList{
+    
+    var head: ListNode?
+    func insert(){
+        
+    }
+    
+    func displayList(){
+     print("LIST: ")
+    var current = head
+        while current != nil {
+            print(current?.val ?? "")
+            current = current?.next
+        }
+        
+    }
+    
+    // 1->2->3-nil
+    func setupDummyNodes(){
+       let two = ListNode(1, nil)
+       let one = ListNode(0, two)
+        head = ListNode(1, one)
+    }
+    
+
+    
+    
+}
+
+
+
+ 
+class Solution {
+    func getDecimalValue(_ head: ListNode?) -> Int {
+        var current = head
+        var sum = 0
+        while current != nil{
+            sum = sum*2 + current!.val
+            current = current?.next
+        }
+       return sum
+        
+    }
+    func convertToDecimal(){
+        let arr = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+        var sum = 0
+        
+        for i in arr{
+             sum = sum*2 + i
+
+            
+        }
+        print("SUM", sum)
+        
+        
+    }
+    
+
+
+}
+
+
+
+
+
+
+
+let sampleList = LinkedList()
+sampleList.setupDummyNodes()
+sampleList.displayList()
+//sampleList.convertToDecimal()
+
+
+
+let solution = Solution()
+
+print(solution.getDecimalValue(sampleList.head))
+
