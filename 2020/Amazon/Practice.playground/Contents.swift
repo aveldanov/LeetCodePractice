95eb19cafc507d510bd87bf124126e2a980543b3
Anton Veldanov <anton.veldanov@gmail.com>
2020-12-02 18:48:08 -0800

    iOS

--- 2020/Amazon/Practice.playground/Contents.swift
+++ 2020/Amazon/Practice.playground/Contents.swift
@@ -1,39 +1,139 @@
-class LinkNode<T>{
-    var next : LinkNode?
-    var value : T
-    
-    init(_ value: T, _ next: LinkNode?) {
-        self.value = value
-        self.next = next
+/*
+ According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+
+ Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+
+ Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+ Any live cell with two or three live neighbors lives on to the next generation.
+ Any live cell with more than three live neighbors dies, as if by over-population..
+ Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+ Write a function to compute the next state (after one update) of the board given its current state. The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously.
+
+ Example:
+
+ Input:
+ [
+   [0,1,0],
+   [0,0,1],
+   [1,1,1],
+   [0,0,0]
+ ]
+ Output:
+ [
+   [0,0,0],
+   [1,0,1],
+   [0,1,1],
+   [0,1,0]
+ ]
+ Follow up:
+
+ Could you solve it in-place? Remember that the board needs to be updated at the same time: You cannot update some cells first and then use their updated values to update other cells.
+ In this question, we represent the board using a 2D array. In principle, the board is infinite, which would cause problems when the active area encroaches the border of the array. How would you address these problems?
+ */
+
+
+class Solution {
+    func gameOfLife(_ board:  [[Int]]) -> [[Int]]{
+        let column = board.count
+        let row = board[0].count
+        var board = board
+        var count = 0
+        for i in 0..<column{
+            for j in 0..<row {
+                
+                print(helper(&board, i: 0, j: 0, count: &count))
+                
+                if board[i][j] == 1{
+//                    print("good",board)
+                    var neighbors = 0
+                    
+                    
+//                    neighbors += checkCell(&board, i: i-1, j: j-1)
+//                    neighbors += checkCell(&board, i: i-1, j: j)
+//                    neighbors += checkCell(&board, i: i-1, j: j+1)
+//                    neighbors += checkCell(&board, i: i, j: j-1)
+//                    neighbors += checkCell(&board, i: i, j: j+1)
+//                    neighbors += checkCell(&board, i: i+1, j: j-1)
+//                    neighbors += checkCell(&board, i: i+1, j: j)
+//                    neighbors += checkCell(&board, i: i+1, j: j+1)
+
+                    if neighbors < 2 || neighbors > 3{
+                        board[i][j]  = 666666 //dies
+                    }
+//                    print("good",neighbors)
+//                    print("")
+                }else{
+                    var neighbors = 0
+//                    print("bad",board)
+
+//                    neighbors += checkCell(&board, i: i-1, j: j-1)
+//                    neighbors += checkCell(&board, i: i-1, j: j)
+//                    neighbors += checkCell(&board, i: i-1, j: j+1)
+//                    neighbors += checkCell(&board, i: i, j: j-1)
+//                    neighbors += checkCell(&board, i: i, j: j+1)
+//                    neighbors += checkCell(&board, i: i+1, j: j-1)
+//                    neighbors += checkCell(&board, i: i+1, j: j)
+//                    neighbors += checkCell(&board, i: i+1, j: j+1)
+                    
+                    if neighbors == 3{
+                        board[i][j]  = 999999
+                    }
+//                    print("bad",neighbors)
+//                    print("")
+                }
+            }
+        }
+        
+        
+//        print(board)
+        for i in 0..<column{
+            for j in 0..<row{
+                if board[i][j] == 666666{
+                    board[i][j] = 0
+                }else if board[i][j] == 999999{
+                    board[i][j] = 1
+                }
+            }
+        }
+        return board
     }
-}
-
-
-class LinkedList<T>{
-   var head: LinkNode<T>?
     
-    
-    func push(_ value:T){
-        if head == nil{
-            head = LinkNode(value, nil)
+    func checkCell(_ board: inout [[Int]], i:Int, j:Int) -> Int{
+//        print("BOOM")
+        if i < 0 || i >= board.count || j < 0 || j >= board[0].count {
+            return 0
+        }
+        if board[i][j] == 0 || board[i][j] == 999999{// will live but still dead
+            return 0
+        }
+        return 1
+
+    }
+
+    func helper(_ board: inout [[Int]], i:Int, j:Int, count:inout Int){
+        
+        print("BOOM")
+        if i < 0 || i >= board.count || j < 0 || j >= board[0].count {
+            print("OUT")
+            return
+        }
+        if board[i][j] == 0 || board[i][j] == 999999{// will live but still dead
+            print("ZERO")
             return
         }
         
-        var current = head
-        while current?.next != nil {
-            current = current?.next
-        }
+        count+=1
         
-        current?.next = LinkNode(value, nil)
+        helper(&board, i: i+1, j: j, count: &count)
+        helper(&board, i: i-1, j: j, count: &count)
+        helper(&board, i: i, j: j+1, count: &count)
+        helper(&board, i: i, j: j-1, count: &count)
+        print(">>>>",count)
+
         
-    }
-    
-    func displayList(){
-        var current = head
-        while current != nil {
-            print(current?.value ?? "")
-            current = current?.next
-        }
+        
+      
+
     }
     
     
@@ -42,10 +142,13 @@ class LinkedList<T>{
 }
 
 
-let linkedList = LinkedList<Int>()
 
-linkedList.push(3)
-linkedList.push(2)
-linkedList.push(1)
+let solution = Solution()
 
-linkedList.displayList()
+print(solution.gameOfLife([
+    [1,1,0],
+    [1,1,1],
+    [1,1,1],
+    [0,0,0]
+  ]))
+
